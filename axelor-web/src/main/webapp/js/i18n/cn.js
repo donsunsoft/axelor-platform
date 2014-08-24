3cdb69165296a5d3c314f747362321e5306b0416
sandwind <sandwind@163.com>
2014-08-25 00:05:50 +0800

    Signed-off-by: sandwind <sandwind@163.com>

---
+++ axelor-web/src/main/webapp/js/i18n/cn.js
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Common Public
+ * Attribution License Version 1.0 (the “License”); you may not use
+ * this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://license.axelor.com/.
+ *
+ * The License is based on the Mozilla Public License Version 1.1 but
+ * Sections 14 and 15 have been added to cover use of software over a
+ * computer network and provide for limited attribution for the
+ * Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an “AS IS”
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is part of "Axelor Business Suite", developed by
+ * Axelor exclusively.
+ *
+ * The Original Developer is the Initial Developer. The Initial Developer of
+ * the Original Code is Axelor.
+ *
+ * All portions of the code written by Axelor are
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ */
+_t.calendar = {
+	monthNames: ['一月','二月','三月','四月','五月','六月', '七月','八月','九月','十月','十一月','十二月'],
+	monthNamesShort: ['一','二','三','四','五','六', '七','八','九','十','十一','十二'],
+	dayNames: ['周日','周一','周二','周三','周四','周五','周六'],
+	dayNamesShort: ['日','一','二','三','四','五','六'],
+	dayNamesMin: ['日','一','二','三','四','五','六'],
+	weekHeader: '周',
+	hourText: '时',
+	minuteText: '分',
+	secondText: '秒',
+	currentText: "今天",
+	closeText: '选择',
+	prevText: '上一页',
+	nextText: '下一页',
+	firstDay: 1
+};
+
+_t.put({
+"About": "关于",
+"Action": "操作",
+"Action Category": "操作分类",
+"Action Sub-Category": "操作子类",
+"Add": "添加",
+"Advanced Search": "高级搜索",
+"All Day": "全天",
+"Archived": "存档",
+"Back": "返回",
+"Cancel": "取消",
+"Clear": "清除",
+"Close": "关闭",
+"Concurrent updates error.": "并发更新错误。",
+"Copy": "复制",
+"Current changes will be lost. Do you really want to proceed?": "当前修改将丢失。您确定要继续操作吗？",
+"Day": "日",
+"Delete": "删除",
+"Do you really want to delete the selected record(s)?": "您确定要删除选中的记录吗？",
+"Do you really want to update all {0} records?": "您确定要更新这{0}条记录吗？",
+"Download": "下载",
+"Edit": "编辑",
+"Edit event": "编辑事件",
+"Error": "错误",
+"Export": "导出",
+"Failed!": "失败",
+"Go": "开始",
+"Grouped": "分组",
+"Help": "帮助",
+"Home": "首页",
+"Information": "信息",
+"Internal Server Error": "服务器错误",
+"Loading": "正在加载",
+"Login": "登陆",
+"Login attempt failed.": "登陆尝试失败。",
+"Month": "月",
+"Navigation": "导航",
+"New": "新建",
+"No Data Available.": "没有可用的数据。",
+"Objects": "对象",
+"OK": "确定",
+"Offline!": "离线",
+"Password": "密码",
+"Profile": "简介",
+"Question": "问题",
+"Refresh": "刷新",
+"Remove": "删除",
+"Save": "保存",
+"Search": "搜索",
+"Select": "选择",
+"Server is not responding.": "服务器没有响应。",
+"Show archived": "显示存档",
+"Show Details": "显示详情",
+"Sign Out": "登出",
+"Stacked": "堆放",
+"Today": "今天",
+"User": "用户",
+"Version:": "版本：",
+"Warning": "警告",
+"Week": "周",
+"{0} to {1} of {2}": "{0} 至 {1} 共 {2}",
+"{0} of {1}": "{0} 至 {1}",
+"Orig. Value": "原值",
+"Created On:": "创建时间：",
+"Created By:": "创建人：",
+"Update": "更新",
+"Update all": "全部更新",
+"Mass Update": "批量更新",
+"Updated On:": "更新时间：",
+"Updated By:": "更新人：",
+"Update Log": "更新日志",
+"Upload": "上传",
+"Unknown": "未知",
+"The record has been updated or delete by another action.": "这条记录已被其他操作更新或修改。",
+"Would you like to reload the current record?": "您想重新加载当前记录吗？",
+"You are not allow to upload a file bigger than": "允许上传的文件最大不能超过",
+"equals": "等于",
+"not equal": "不等于",
+"greater or equal": "大于或等于",
+"less or equal": "小于或等于",
+"greater than": "大于",
+"less than": "小于",
+"contains": "包含",
+"doesn't contain": "不包含",
+"in range": "在...内",
+"not in range": "不在...内",
+"is null": "为空",
+"is not null": "不为空",
+"is true": "为真",
+"is false": "为假",
+"and": "且",
+"or": "或",
+"Add filter": "添加过滤器",
+"Apply": "应用",
+"Would you like to remove the filter?": "您确定要删除这个过滤器？",
+"Filters": "过滤器",
+"My Filters": "我的过滤器",
+"Share": "共享",
+"Save as": "另存为",
+"Delete": "删除",
+"Save filter as": "保存过滤器",
+"({0} items)": "({0} 条记录)",
+"and {0} more": "还有 {0} 条记录",
+"Object": "对象",
+"Field Name": "字段名称",
+"Field Type": "字段类型",
+"Filter": "过滤器",
+"Reference": "参照",
+"Create \"{0}\" and add...": "创建 \"{0}\" 并添加...",
+"Create \"{0}\"...": "创建 \"{0}\"...",
+"Search...": "搜索...",
+"Create...": "创建...",
+"Sort Ascending": "升序",
+"Sort Descending": "降序",
+"Group by": "分组",
+"Ungroup": "取消分组",
+"Hide": "隐藏",
+"Show": "显示",
+"Logout": "注销",
+"Preferences": "首选项",
+"Create new": "新建"
+});
